811ccd6583761205610cb6876e29a8b114cfc0c7
Dean Cahill <dpcahill98@gmail.com>
2024-11-01 13:27:32 -0400

    problem6

--- euler-problems-clj/src/euler_problems_clj/1_50.clj
+++ euler-problems-clj/src/euler_problems_clj/1_50.clj
@@ -3,7 +3,6 @@
 
 ;;===========================================================================|
 ;; Problem One
-
 (defn problem-one
   "find the sum of all multiples of 3 or 5 below 1000"
   [upper-bound]
@@ -36,8 +35,7 @@
 
 ;;===========================================================================|
 ;; Problem Three
-
-(defn gen-primes "Generates an infinite, lazy sequence of prime numbers"
+(defn gen-primes
   []
   (letfn [(reinsert [table x prime]
             (update-in table [(+ prime x)] conj prime))
@@ -56,12 +54,26 @@
   (let [primes (gen-primes)]
     (last (filter #(= (mod n %) 0) (take-while #(< % (math/sqrt n)) primes)))))
 
-(problem-three 13195)
-
 (problem-three 600851475143)
 
 ;;===========================================================================|
-;; Problem Four -- Palindrome Product
+;; TODO Problem Four -- Palindrome Product
+(defn naive-palindrome-search [lower upper]
+  (loop [u upper acc []]
+    (if (= lower u)
+      acc
+      (let [prod (* lower u)
+            prod-rep (seq (str prod))]
+        (if (= (reverse prod-rep) prod-rep)
+          (recur (dec u) (conj acc prod))
+          (recur (dec u) acc))))))
+
+(defn palindrome-search [lower upper]
+  ())
+
+(palindrome-search 10 99)
+(palindrome-search 900 999)   ;; ?? pattern does not include ...99
+(palindrome-search 1000 9999)
 
 (defn problem-four
   "Find the largest palindrome made from
@@ -69,18 +81,26 @@
   [])
 
 ;;===========================================================================|
-;; Problem Five -- Smallest Multiple
+;; TODO Problem Five -- Smallest Multiple
 (defn factorize [n]
-  (let [primes (gen-primes)]
-    (loop [primes primes n n]
-      (let [p (first primes)]
-        (if (= (mod n p) 0)
-          (recur primes (/ n p))
-          (recur (rest primes) n))))))
+  (loop [n n acc [1] primes (gen-primes)]
+    (if (= n 1)
+      acc
+      (let [p-factor (first primes)]
+        (if (= 0 (mod n p-factor))
+          (recur (quot n p-factor) (conj acc p-factor) primes)
+          (recur n acc (rest primes)))))))
 
-(factorize 10) ;; EXPECTS -> 2,5
-(factorize 9) ;; expects -> 3,3
-(factorize 2520) ;;expects -> 2,2,2,3,3,5,7
+(defn prime-factorization [n]
+  (let [factors (factorize n) d {}]
+    (reduce (fn [dict val]
+              (if (dict val)
+                (assoc dict val (+ 1 (dict val)))
+                (assoc dict val 1)))
+            d factors)))
+
+;; (f [{a 1 b 2 c 3} {a 2 b 1 c 3} {a 1 b 1 c 1}]) := {a 2 b 2 c 3}
+;; given a vector of dicts, return a dict with the maximum values for each key
 
 (defn problem-five
   "finds the smallest number evenly divisible
@@ -88,3 +108,39 @@
   [upper-bound])
 
 (problem-five 10)  ;;EXPECTS -> 2520
+(problem-five 20)  ;;expects -> ??
+
+;;===========================================================================|
+;; Problem Six -- Sum Square Difference
+(defn sum-sq-diff [lim]
+  (let [vals (range 1 (+ 1 lim))
+        sqsum (math/pow (reduce + vals) 2)
+        sumsq (reduce + (map #(math/pow % 2) vals))]
+    (- sqsum sumsq)))
+
+;;===========================================================================|
+;; Problem Seven -- 10,001st Prime
+(nth (gen-primes) 10000) ;;0-indexing
+
+;;===========================================================================|
+;; TODO Problem Eight -- Largest Product in a Series
+
+;;===========================================================================|
+;; TODO Problem Nine -- Special Pythagorean Triple
+
+;;===========================================================================|
+;; TODO Problem Ten --
+
+;;===========================================================================|
+;; TODO Problem Eleven --
+
+;;===========================================================================|
+;; TODO Problem Twelve -- Highly Divisible Triangular Number
+(defn triangle-number
+  [n]
+  (reduce + (take n (iterate inc 1))))
+
+;; > 500 divisors
+
+;;===========================================================================|
+;; TODO Problem Thirteen -- Large Sum
